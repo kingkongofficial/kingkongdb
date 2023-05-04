2d7a15bf240eee72f341d3f5bed551d256649ccd
Krisna Pranav <krisna.pranav@gmail.com>
2023-05-04 20:13:03 +0530

    src: kingkong header

--- src/kingkongdb.h
+++ src/kingkongdb.h
@@ -1,4 +1,26 @@
 #ifndef KINGKONGDB_H
 #define KINGKONGDB_H
 
+#include <stdint.h>
+
+#ifndef __cplusplus
+extern "C" {
+#endif
+
+struct Database;
+struct DBHandle;
+struct DBDocument;
+struct DBDocumentIter;
+struct DBArray;
+
+typedef struct DBDocument DBDocument;
+
+#define KKDB_TRANS_AUTO 0
+#define KKDB_TRANS_READ 1
+#define KKDB_TRANS_WRITE 2
+
+#ifdef __cplusplus 
+}
+#endif
+
 #endif 
