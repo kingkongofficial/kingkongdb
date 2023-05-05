d1238c2e7ece4c9a0b799d828dd30d378a751752
Krisna Pranav <krisna.pranav@gmail.com>
2023-05-05 17:12:54 +0530

    kingkongdb_core: commands

--- src/kingkongdb_core/commands.rs
+++ src/kingkongdb_core/commands.rs
@@ -9,46 +9,45 @@
  * 
  */
 
-
 use bson::Document;
 use bson::oid::ObjectId;
-use serde::{Serialize, Deseralize};
+use serde::{Serialize, Deserialize};
 use crate::TransactionType;
 
-#[derive(Serialize, Deseralize)]
+#[derive(Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct FindCommandOptions {
     pub session_id: Option<ObjectId>,
 }
 
-#[derive(Serialize, Deseralize)]
+#[derive(Serialize, Deserialize)]
 pub struct FindCommand {
     pub ns: String,
     pub multi: bool,
     pub filter: Option<Document>,
-    pub options: Option<FindCommandOptions>
+    pub options: Option<FindCommandOptions>,
 }
 
-#[derive(Serialize, Deseralize)]
+#[derive(Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct InsertCommandOptions {
     pub session_id: Option<ObjectId>,
 }
 
-#[derive(Serialize, Deseralize)]
+#[derive(Serialize, Deserialize)]
 pub struct InsertCommand {
     pub ns: String,
     pub documents: Vec<Document>,
     pub options: Option<InsertCommandOptions>,
 }
 
-#[derive(Serialize, Deseralize)]
+#[derive(Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct UpdateCommandOptions {
-    pub session_id: Option<ObjectId>
+    pub session_id: Option<ObjectId>,
 }
 
-#[derive(Serialize, Deseralize)]
+#[derive(Serialize, Deserialize)]
 pub struct UpdateCommand {
     pub ns: String,
     pub filter: Document,
@@ -57,25 +56,95 @@ pub struct UpdateCommand {
     pub options: Option<UpdateCommandOptions>,
 }
 
-#[derive(Serialize, Deseralize)]
+#[derive(Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct DeleteCommandOptions {
     pub session_id: Option<ObjectId>,
 }
 
-#[derive(Serialize, Deseralize)]
+#[derive(Serialize, Deserialize)]
 pub struct DeleteCommand {
     pub ns: String,
     pub filter: Document,
     pub multi: bool,
-    pub options: Option<DeleteCommandOptions>
+    pub options: Option<DeleteCommandOptions>,
 }
 
-#[derive(Serialize, Deseralize)]
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct CreateCollectionCommandOptions {
+    pub session_id: Option<ObjectId>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct CreateCollectionCommand {
+    pub ns: String,
+    pub options: Option<CreateCollectionCommandOptions>,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct DropCollectionCommandOptions {
+    pub session_id: Option<ObjectId>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct DropCollectionCommand {
+    pub ns: String,
+    pub options: Option<DropCollectionCommandOptions>,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct CountDocumentsCommandOptions {
+    pub session_id: Option<ObjectId>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct CountDocumentsCommand {
+    pub ns: String,
+    pub options: Option<CountDocumentsCommandOptions>,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct StartTransactionCommand {
+    pub ty: Option<TransactionType>,
+    pub session_id: ObjectId,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct CommitTransactionCommand {
+    pub session_id: ObjectId,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AbortTransactionCommand {
+    pub session_id: ObjectId,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct DropSessionCommand {
+    pub session_id: ObjectId,
+}
+
+#[derive(Serialize, Deserialize)]
 #[serde(tag = "command")]
 pub enum CommandMessage {
     Find(FindCommand),
     Insert(InsertCommand),
     Update(UpdateCommand),
     Delete(DeleteCommand),
+    CreateCollection(CreateCollectionCommand),
+    DropCollection(DropCollectionCommand),
+    CountDocuments(CountDocumentsCommand),
+    StartTransaction(StartTransactionCommand),
+    CommitTransaction(CommitTransactionCommand),
+    AbortTransaction(AbortTransactionCommand),
+    StartSession,
+    DropSession(DropSessionCommand),
+    SafelyQuit,
 }
